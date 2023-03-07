322b21f0ce5e5928871670544137dc00fe9f1794
BaptisteLac <baptiste.lacoste0@gmail.com>
2023-03-07 11:39:50 +0100

    Todo functionality done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Item } from "./item";
+
 
 @Component({
   selector: 'app-root',
@@ -24,6 +26,10 @@ export class AppComponent {
       done: false
     });
   }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }  
 
   get items() {
     if (this.filter === 'all') {
